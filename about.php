23aad2a177836d5f488b53d5d13088c9d8362f62
Anand <63843910+anandkumar190@users.noreply.github.com>
2020-10-01 17:24:18 +0530

    Update about.php
    Heading  and all col add

--- about.php
+++ about.php
@@ -3,7 +3,8 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h3 class="page-header">About us</h3>                            
+                    <h1 class="page-header">About us</h1>    
+                        
                 </div>
             </div>
           <div class="row">
@@ -18,20 +19,20 @@
 <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
 
 <div class="form-group">
-   <label class="control-label col-sm-2" for="product_name">Product Name: <span class="text-danger"><b>*</b></span></label>
-      <div class="col-sm-10">
+   <label class="control-label  col-lg-2 col-md-2 col-sm-2 col-xs-2  " for="product_name"> Product Name: <span class="text-danger"><b>*</b></span></label>
+      <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 ">
         <input type="text" name="title" value="<?php echo $title; ?>" class="form-control">
       </div>
 </div>
 <div class="form-group">
-      <div class="col-sm-12">
+      <div class=" col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <textarea class="form-control" rows="2" name="content"><?php echo $content; ?></textarea>
         <script>CKEDITOR.replace( 'content' );</script>
       </div>
 </div>
 
     <div class="form-group">        
-      <div class="col-sm-offset-0 col-sm-8">
+      <div class="col-sm-offset-0 col-lg-10 col-md-10 col-sm-10 col-xs-10 ">
         <button type="submit" name="UpdateAboutUs" id="UpdateAboutUs" class="btn btn-primary">Submit</button>
         <a href="" name="reset" class="btn btn-primary">Reset</a>
       </div>
